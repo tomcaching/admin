d1d50765a32033e2873ca16f3463974855800044
Jiří Vrba <jiri@vrba.dev>
2023-01-27 12:53:55 +0100

    Replace the default Next.js assets

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <title>Tomcaching admin</title>
         <meta name="description" content="Administrace kešek pro Tomcaching" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
     </>
   )
